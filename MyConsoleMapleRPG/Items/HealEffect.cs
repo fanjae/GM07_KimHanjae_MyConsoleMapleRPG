8cefbb257fd708ec02f802003df6d5839cfd5424
FanJae <hjjang45@gmail.com>
2026-05-18 23:16:23 +0900

    feat : 아이템 사용 효과 구현

---
+++ MyConsoleMapleRPG/Items/HealEffect.cs
@@ -0,0 +1,21 @@
+﻿using MyConsoleMapleRPG.Character;
+
+namespace MyConsoleMapleRPG.Items.Effects
+{
+    // HP를 회복 시키는 아이템 효과
+    // ItemEffect를 구현하여, ItemData의 Effect로 공통 방식으로 사용할 수 있다.
+    internal class HealEffect : IItemEffect
+    {
+        private readonly int amount;
+
+        public HealEffect(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public void Apply(Player player)
+        {
+            player.Heal(amount);
+        }
+    }
+}
---
+++ MyConsoleMapleRPG/Items/RestoreMpEffect.cs
@@ -0,0 +1,20 @@
+﻿using MyConsoleMapleRPG.Character;
+
+namespace MyConsoleMapleRPG.Items.Effects
+{
+    // MP를 회복 시키는 아이템 효과
+    internal class RestoreMpEffect : IItemEffect
+    {
+        private readonly int amount;
+
+        public RestoreMpEffect(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public void Apply(Player player)
+        {
+            player.RestoreMp(amount);
+        }
+    }
+}
