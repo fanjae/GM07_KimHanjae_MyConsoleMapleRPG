6ce14450568d21b7f59c1832beaa0132d2ea2e5a
FanJae <hjjang45@gmail.com>
2026-05-18 23:25:54 +0900

    feat : 배경음, 효과음 시스템 구현

---
+++ MyConsoleMapleRPG/UI/Audio/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Media;
+namespace MyConsoleMapleRPG.UI.Audio
+{
+    internal static class AudioManager // 오디오 매니저
+    {
+        private static SoundPlayer? currentBgm;
+        private static string? currentPath;
+
+        public static void PlayBgm(string path)
+        {
+            if (currentPath == path)
+                return;
+
+            StopBgm(); // 기존 재생 중인 Bgm은 끔
+
+            currentBgm = new SoundPlayer(path);
+            currentBgm.PlayLooping();
+            currentPath = path;
+        }
+
+        public static void StopBgm()
+        {
+            currentBgm?.Stop();
+            currentBgm = null;
+            currentPath = null;
+        }
+
+        public static void PlaySfx(string path)
+        {
+            SoundPlayer sfx = new SoundPlayer(path);
+            sfx.Play();
+        }
+    }
+}
