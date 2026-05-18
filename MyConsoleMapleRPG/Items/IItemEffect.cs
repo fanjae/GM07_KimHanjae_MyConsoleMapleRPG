00cc3b76a612fef77fc76c9c130106804a0c54ee
FanJae <hjjang45@gmail.com>
2026-05-18 23:20:38 +0900

    feat : 아이템 효과 관련 인터페이스 구현

---
+++ MyConsoleMapleRPG/Items/IItemEffect.cs
@@ -0,0 +1,11 @@
+﻿using MyConsoleMapleRPG.Character;
+
+namespace MyConsoleMapleRPG.Items.Effects
+{
+    // 소비 아이템 효과의 공통 인터페이스
+    // 효과 종류가 달라도 Apply(Player)하나로 실행할 수 있게 함.
+    internal interface IItemEffect
+    {
+        void Apply(Player player);
+    }
+}
