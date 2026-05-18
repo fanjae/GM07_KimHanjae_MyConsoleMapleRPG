31f400054d747ea1061e18c30202b17805a04e95
FanJae <hjjang45@gmail.com>
2026-05-18 23:25:30 +0900

    feat : 게임 저장/불러오기 시스템 구현

---
+++ MyConsoleMapleRPG/Save/InventorySaveData.cs
@@ -0,0 +1,9 @@
+﻿namespace MyConsoleMapleRPG.Save
+{
+    internal class InventorySaveData // 인벤토리 저장 정보
+    {
+        public int ItemId { get; set; }
+        public int Quantity { get; set; }
+        public bool IsEquipped { get; set; }
+    }
+}
---
+++ MyConsoleMapleRPG/Save/SaveData.cs
@@ -0,0 +1,23 @@
+﻿namespace MyConsoleMapleRPG.Save
+{
+    internal class SaveData // 플레이어 저장 정보
+    {
+        public string PlayerType { get; set; }
+
+        public int Level { get; set; }
+        public int Exp { get; set; }
+
+        public int Hp { get; set; }
+        public int Mp { get; set; }
+
+        public int Gold { get; set; }
+
+        public int MaxHp { get; set; }
+        public int MaxMp { get; set; }
+
+        public int BaseAttack { get; set; }
+        public int BaseDefense { get; set; }
+
+        public List<InventorySaveData> InventoryItems { get; set; }
+    }
+}
