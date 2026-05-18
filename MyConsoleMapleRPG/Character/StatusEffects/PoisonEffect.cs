d3d84f9badf3d1a5938f419d584f9bded44822e4
FanJae <hjjang45@gmail.com>
2026-05-18 23:19:06 +0900

    feat : 캐릭터 상태이상 관련 효과 구현

---
+++ MyConsoleMapleRPG/Character/StatusEffects/PoisonEffect.cs
@@ -0,0 +1,20 @@
+﻿namespace MyConsoleMapleRPG.Character.StatusEffects
+{
+    internal class PoisonEffect : StatusEffect
+    {
+        private readonly int damage;
+
+        public PoisonEffect(int damage, int remainingTurns) : base("중독", remainingTurns)
+        {
+            this.damage = damage;
+        }
+
+        public override string Apply(Character target) // 중독에 대한 도트템 적용
+        {
+            int finalDamage = target.TakeFixedDamage(damage);
+            RemainingTurns--;
+
+            return $"{target.Name}은(는) 중독으로 {finalDamage}의 피해를 입었습니다.";
+        }
+    }
+}
---
+++ MyConsoleMapleRPG/Character/StatusEffects/StatusEffect.cs
@@ -0,0 +1,18 @@
+﻿namespace MyConsoleMapleRPG.Character.StatusEffects
+{
+    internal abstract class StatusEffect
+    {
+        public string Name { get; }
+        public int RemainingTurns { get; protected set; } // 남은 턴
+
+        protected StatusEffect(string name, int remainingTurns)
+        {
+            Name = name;
+            RemainingTurns = remainingTurns;
+        }
+
+        public bool IsExpired => RemainingTurns <= 0; // 0하면 만료 처리
+
+        public abstract string Apply(Character target);
+    }
+}
