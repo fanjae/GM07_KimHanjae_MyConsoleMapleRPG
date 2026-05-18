60d32691d55e653e7ec6e5e695ab10e4ea26f5ee
FanJae <hjjang45@gmail.com>
2026-05-18 23:03:37 +0900

    feat : 플레이어 직업 선택 관련 화면 및 입력 처리 구현

---
+++ MyConsoleMapleRPG/UI/Screens/CharacterSelectScreen.cs
@@ -0,0 +1,50 @@
+﻿using MyConsoleMapleRPG.UI;
+using MyConsoleMapleRPG.UI.Audio;
+using MyConsoleMapleRPG.UI.Views;
+
+namespace MyConsoleMapleRPG.UI.Screens
+{
+    // 직업 선택 화면의 입력 흐름을 담당하는 Screen 클래스
+    // 실제 출력은 CharacterSelectview에 위임. 클래스는 선택 인덱스와 입력 처리만 관리
+
+    internal class CharacterSelectScreen
+    {
+        private readonly CharacterSelectView view = new CharacterSelectView();
+
+        private readonly string[] menuItems =
+        {
+            "전사",
+            "마법사"
+        };
+
+        public int Show() 
+        {
+            int selectedIndex = 0;
+
+            Console.Clear();
+            view.DrawBaseScreen();
+
+            while (true)
+            {
+                view.DrawMenuItems(menuItems, selectedIndex);
+
+                ConsoleKey key = Console.ReadKey(true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.LeftArrow:
+                    case ConsoleKey.RightArrow:
+                        selectedIndex = MenuInput.MoveHorizontal(selectedIndex, menuItems.Length, key);
+                        break;
+
+                    case ConsoleKey.Z:
+                    case ConsoleKey.Enter:
+                        return selectedIndex;
+
+                    case ConsoleKey.Escape:
+                        return -1;
+                }
+            }
+        }
+    }
+}
---
+++ MyConsoleMapleRPG/UI/Views/CharacterSelectView.cs
@@ -0,0 +1,44 @@
+﻿using MyConsoleMapleRPG.UI.Rendering;
+
+namespace MyConsoleMapleRPG.UI.Views
+{  
+    // 직업 선택 화면의 출력 전용 View 클래스
+    // 직업 이미지와 선택 메뉴 출력.
+
+    internal class CharacterSelectView
+    {
+        private const int MenuStartX = 22;
+        private const int MenuY = 36;
+        private const int MenuSpacing = 45;
+        private const int MenuWidth = 14;
+
+        public void DrawBaseScreen() // 직업 선택 관련 기본 창 그림
+        {
+            ConsoleRenderer.DrawWindow(2, 1, 90, 40, "JOB SELECT");
+
+            Console.SetCursorPosition(38, 3);
+            Console.Write("직업을 선택하세요");
+
+            AsciiImageRenderer.Draw("Assets/image/sword.png", 10, 8);
+            AsciiImageRenderer.Draw("Assets/image/staff.png", 45, 8);
+
+            Console.Write("\x1b[0m");
+            Console.ResetColor();
+        }
+
+        public void DrawMenuItems(string[] menuItems, int selectedIndex) // 메뉴 창 그림 (전사, 마법사 글자)
+        {
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                Console.SetCursorPosition(MenuStartX + i * MenuSpacing, MenuY);
+
+                string text = i == selectedIndex ? $"▶ {menuItems[i]}" : $" {menuItems[i]}"; // 선택 유무에 따라 화살표 표기
+
+                Console.Write(text.PadRight(MenuWidth));
+            }
+
+            Console.Write("\x1b[0m");
+            Console.ResetColor();
+        }
+    }
+}
