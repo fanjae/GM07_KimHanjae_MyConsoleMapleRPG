c735822827fe4abfcf5b4effa72dcc736ccf9cbd
FanJae <hjjang45@gmail.com>
2026-05-18 23:27:29 +0900

    init : MyConsoleRPG 프로젝트 init

---
+++ MyConsoleMapleRPG/Program.cs
@@ -0,0 +1,18 @@
+﻿using MyConsoleMapleRPG.Systems;
+using MyConsoleMapleRPG.UI.Settings;
+
+namespace MyConsoleMapleRPG
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            ConsoleSetting.ConsoleLock();
+
+            GameController game = new GameController();
+            game.Run();
+        }
+    }
+}
